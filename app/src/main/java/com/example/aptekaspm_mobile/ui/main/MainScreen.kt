a4d159c6da95fdcd83855b1c8d11e652453c7149
Pavel Kholkin <xpaul6@gmail.com>
2025-11-04 16:26:40 +0500

    ref: demo main screen layout

--- app/src/main/java/com/example/aptekaspm_mobile/ui/main/MainScreen.kt
+++ app/src/main/java/com/example/aptekaspm_mobile/ui/main/MainScreen.kt
@@ -2,30 +2,43 @@ package com.example.aptekaspm_mobile.ui.main
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.aptekaspm_mobile.ui.navigation.Screen
 
 @Composable
 fun MainScreen(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+            .padding(bottom = 16.dp),
+        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { navController.navigate(Screen.Receive.route) }) {
-            Text("Receive Medicine")
-        }
-        Button(onClick = { navController.navigate(Screen.Restock.route) }) {
-            Text("Restock Medkit")
-        }
-        Button(onClick = { navController.navigate(Screen.Logs.route) }) {
-            Text("View Logs")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { navController.navigate(Screen.Receive.route) }) {
+                Text("Receive")
+            }
+            Button(onClick = { navController.navigate(Screen.Logs.route) }) {
+                Text("Logs")
+            }
+            Button(onClick = { navController.navigate(Screen.Restock.route) }) {
+                Text("Restock")
+            }
         }
     }
 }
